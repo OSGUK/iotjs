a97b2f5e505ea3f1af3aa9b5e4a254f2ffbb4f86
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2018-02-07 17:29:26 +0000

    created python file to make a config file of the local machine

---
+++ http_demo/config/ip_get.py
@@ -0,0 +1,54 @@
+# Copyright 2015-present Samsung Electronics Co., Ltd. and other contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+'''
+    This program is designed to run, get the IP address from the local system and store in a config file
+    on the local machine. The program will be invoked via a cron job.
+
+    Author: Nicholas Herriot
+
+    To install:
+        /> pip install netifaces
+        or
+        /> pip install -r requirements.txt
+'''
+
+from netifaces import interfaces, ifaddresses, AF_INET, AF_LINK
+import json
+
+# Create an empty dictionary to store machine network config, network interfaces (e.g. 'lo','eth0' and 'wlan0').
+# Create ip address list and network interfaces list.
+machine_interface_dict={}
+ip_addresses = []
+mac_addresses = []
+network_interfaces = interfaces()
+file_name = 'machine_net_interface.json'            # File name of json file
+
+for ifaceName in interfaces():
+    ip_addresses.append([i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr': 'No IP addr'}])])
+
+for ifaceName in interfaces():
+    mac_addresses.append([i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_LINK, [{'addr': 'No IP addr'}])])
+
+
+machine_interface_dict = dict(zip(network_interfaces, zip(ip_addresses, mac_addresses)))
+
+print('***** Found network interfaces *****')
+
+with open(file_name, 'w') as fp:
+    json.dump(machine_interface_dict, fp)
+
+print('***** File Written *****')
+print(' ---- File name: {}'.format(file_name))
