489b9774015079744217d93e20f11f583b33255d
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2018-02-07 15:35:02 +0000

    Making the server file parse urls to know when to send a html page or switch a light

--- http_demo/server_html.js
+++ http_demo/server_html.js
@@ -15,17 +15,53 @@
 
 var http = require('http');
 var port = 8080;
+var path = '/light';
 
 var fs = require('fs');
+var netiface = require('dns');
 
-http.createServer(function (req, res) {
- // The path for onoff.html is relevant. Here I assume that it's in the same directory of 
- // the server file and iotjs binary. Change it in relevant to your iotjs binary path
- // Otherwise, put server_html.js and onoff.html in the same directory and have a symoblic
- // link of iotjs binary in that directory as well.
- fs.readFile('onoff.html',function (err, data){
-        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
-        res.write(data);
-        res.end();
+var server = http.createServer(function (req, res)
+{
+     // The path for onoff.html is relevant. Here I assume that it's in the same directory of
+     // the server file and iotjs binary. Change it in relevant to your iotjs binary path
+     // Otherwise, put server_html.js and onoff.html in the same directory and have a symoblic
+     // link of iotjs binary in that directory as well.
+
+    console.log(req.url);
+	var myval = req.url.split('/');
+	console.log(myval[1]);
+	//console.log()
+
+    //netiface.lookupService('127.0.0.1', 22, (err, hostname, service), console.log(hostname, service));
+
+    netiface.lookup('Zenbook-UX32A', function (err, address) {
+        console.log(address);
     });
-}).listen(port);
+
+    //console.log(netiface.localName())
+
+	if (myval[1]=='light'){
+	    console.log('yahoo');
+        fs.readFile('onoff.html',function (err, data){
+            res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+            res.write(data);
+            res.end();
+        });
+
+    }
+    else{
+	    console.log('no way jose');
+        res.writeHead(200, {"Content-Type" : "text/plain"});
+		res.write(req.url);
+		res.end();
+
+    }
+
+     // fs.readFile('onoff.html',function (err, data){
+     //        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+     //        res.write(data);
+     //        res.end();
+     //   });
+});
+
+server.listen(port);
